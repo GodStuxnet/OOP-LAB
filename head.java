eba79e148f60d3c0cfcadc6aad363e265d3f9620
GodStuxnet <ch.sc.u4cse25218@ch.students.amrita.edu>
2025-12-28 12:25:06 +0530

    Add files via upload

---
+++ head.java
@@ -0,0 +1,11 @@
+class Head
+{
+ 
+     public static void main(String args[]){
+
+             System.out.println("Hi Master , Hemraj");
+             System.out.print("What's in your mind \n");
+}
+
+
+}
