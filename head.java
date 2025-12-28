930355ad8c616ea54968962da485743f9bb57586
GodStuxnet <ch.sc.u4cse25218@ch.students.amrita.edu>
2025-12-28 12:30:24 +0530

    Update head.java

--- head.java
+++ head.java
@@ -1,11 +1,12 @@
 class Head
 {
- 
+     // This is my First Java Program
      public static void main(String args[]){
 
              System.out.println("Hi Master , Hemraj");
              System.out.print("What's in your mind \n");
 }
+
 
 
 }
