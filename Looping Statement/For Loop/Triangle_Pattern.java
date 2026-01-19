6396bf09e9bc858b7cc48967ae58131ad23cf394
GodStuxnet <ch.sc.u4cse25218@ch.students.amrita.edu>
2026-01-19 11:48:20 +0530

    Add files via upload

---
+++ Looping Statement/For Loop/Triangle_Pattern.java
@@ -0,0 +1,14 @@
+class Triangle_Pattern{
+
+ public static void main(String[] args){
+
+   String symbol = "*";
+
+   for(int i=1;i<6;i++){
+
+      System.out.println(symbol.repeat(i));
+
+   }
+
+ }
+}
